131126c56f1b5f8620133e25038a89f151133aa0
Alvaro Barrero <barreroalvaro2007@gmail.com>
2023-02-12 04:26:46 +0100

    version 1.1 del ejemplo de decorador en python

--- index.py
+++ index.py
@@ -1,28 +1,29 @@
-from time import time as t
-from time import sleep as s
-from random import random as r
+import time
+from random import randint as rint
  
-def timer(func):
+def func_info(func):
     def wrapper(*args, **kwargs):
-        t1 = t()
+        t1 = time.time()
         result = func(*args, **kwargs)
-        t2 = t()-t1
+        t2 = time.time()-t1
         return f"Llamada a la funcion: {func.__name__}, Argumentos: {args}, {kwargs}, Tiempo en ejecutar: {t2}, resultado: {result}"
     return wrapper
 
 
-@timer
-def esperar(tiempo):
-    # simulando una funcion con mucho codigo que tarde en ejecutarse
-    s(tiempo)
-    return "Its done"
+@func_info
+def esperar(tiempo) -> None:
+    # simulando una funcion con mucho codigo que tarde en ejecutarse y retornando cualquier garbage para que al formar parte del decorador, la misma funcion muestre su info. Info como... lo que retorna
+    time.sleep(tiempo)
+    return None
 
 
-@timer
-def potenciar_num(base, exponente):
-    s(r()*2)
-    return r() < 0.2
+@func_info
+def potenciar_num(base, exponente) -> bool:
+    # simulando una funcion con mucho codigo que tarde en ejecutarse y retornando cualquier garbage para que al formar parte del decorador, la misma funcion muestre su info. Info como... lo que retorna
+    time.sleep(rint() * 2)
+    return rint() < 0.2
 
 
-# print(esperar(1))
+print(esperar(1))
 print(potenciar_num(20, 50))
+print("Its done")
